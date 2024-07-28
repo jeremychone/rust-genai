3330b99dce01b20b9b08af4e6846050f8eeae362
Jeremy Chone <jeremy.chone@gmail.com>
2024-07-28 11:25:49 -0700

    . minor code clean (module scoping)

--- src/adapter/support.rs
+++ src/adapter/support.rs
@@ -4,7 +4,7 @@ use crate::{Error, Result};
 /// Returns the `api_key` value from the config_set auth_resolver
 /// This function should be called if the adapter must have a api_key
 /// Fail if the no auth_resolver or no auth_data
-pub(crate) fn get_api_key_resolver(model_info: ModelInfo, config_set: &ConfigSet<'_>) -> Result<String> {
+pub fn get_api_key_resolver(model_info: ModelInfo, config_set: &ConfigSet<'_>) -> Result<String> {
 	let auth_resolver = config_set
 		.adapter_config()
 		.auth_resolver()
