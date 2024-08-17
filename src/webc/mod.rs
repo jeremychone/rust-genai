ae223ca01c4dd68f13ed0bdb344476aa0d2c3cd6
Jeremy Chone <jeremy.chone@gmail.com>
2024-08-17 12:14:13 -0700

    . makes only webc::Error public (relates to: #12)

--- src/webc/mod.rs
+++ src/webc/mod.rs
@@ -5,8 +5,11 @@ mod web_client;
 // for when not `text/event-stream`
 mod web_stream;
 
-pub use error::{Error, Result};
-pub use web_client::*;
-pub use web_stream::*;
+pub(crate) use error::Result;
+pub(crate) use web_client::*;
+pub(crate) use web_stream::*;
+
+// only public for external use
+pub use error::Error;
 
 // endregion: --- Modules
