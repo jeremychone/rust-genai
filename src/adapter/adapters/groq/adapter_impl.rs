3fc7a31a5826006752f54b1e1a391ade0d897d25
Jeremy Chone <jeremy.chone@gmail.com>
2024-09-28 15:14:52 -0700

    . update groq models (llama 3.2)

--- src/adapter/adapters/groq/adapter_impl.rs
+++ src/adapter/adapters/groq/adapter_impl.rs
@@ -10,6 +10,10 @@ pub struct GroqAdapter;
 
 const BASE_URL: &str = "https://api.groq.com/openai/v1/";
 pub(in crate::adapter) const MODELS: &[&str] = &[
+	"llama-3.2-90b-vision-preview",
+	"llama-3.2-11b-vision-preview",
+	"llama-3.2-3b-preview",
+	"llama-3.2-1b-preview",
 	"llama-3.1-405b-reasoning",
 	"llama-3.1-70b-versatile",
 	"llama-3.1-8b-instant",
--- src/adapter/adapters/groq/mod.rs
+++ src/adapter/adapters/groq/mod.rs
@@ -1,6 +1,6 @@
 //! API Documentation:     https://console.groq.com/docs/api-reference#chat
 //! Model Names:           https://console.groq.com/docs/models
-//! Pricing:              https://wow.groq.com/
+//! Pricing:               https://wow.groq.com/
 
 // region:    --- Modules
 
