a9429ba58168775b87c064e0889989fce0c42beb
Adam Strojek <adam@strojek.info>
2024-12-15 11:45:36 +0100

    Image Support: Fix rebase issue

--- src/adapter/adapters/gemini/adapter_impl.rs
+++ src/adapter/adapters/gemini/adapter_impl.rs
@@ -1,5 +1,5 @@
+use crate::adapter::adapters::support::get_api_key;
 use crate::adapter::gemini::GeminiStreamer;
-use crate::adapter::support::get_api_key;
 use crate::adapter::{Adapter, AdapterKind, ServiceType, WebRequestData};
 use crate::chat::{
 	ChatOptionsSet, ChatRequest, ChatResponse, ChatResponseFormat, ChatRole, ChatStream, ChatStreamResponse,
