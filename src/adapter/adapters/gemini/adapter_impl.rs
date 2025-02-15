fcb98494a5a9ff8082b998e941b669f6f4e0b1ef
Jeremy Chone <jeremy.chone@gmail.com>
2025-02-14 22:08:14 -0800

    . gemini - update model to gemini-2.0-flash-001

--- src/adapter/adapters/gemini/adapter_impl.rs
+++ src/adapter/adapters/gemini/adapter_impl.rs
@@ -17,7 +17,7 @@ pub struct GeminiAdapter;
 
 const MODELS: &[&str] = &[
 	"gemini-1.5-pro",
-	"gemini-2.0-flash-exp",
+	"gemini-2.0-flash-001",
 	"gemini-1.5-flash",
 	"gemini-1.5-flash-8b",
 	"gemini-1.0-pro",
