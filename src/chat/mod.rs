0f6c13390b58961608fbda77e8a87285ec08324b
Himmelschmidt <46351743+Himmelschmidt@users.noreply.github.com>
2025-12-05 15:35:41 -0500

    Capture response body in ResponseFailedNotJson error (#103)
    Fixes #102

--- src/chat/mod.rs
+++ src/chat/mod.rs
@@ -3,26 +3,26 @@
 
 // region:    --- Modules
 
+mod binary;
 mod chat_message;
 mod chat_options;
 mod chat_req_response_format;
 mod chat_request;
 mod chat_response;
 mod chat_stream;
-mod binary;
 mod content_part;
 mod message_content;
 mod tool;
 mod usage;
 
 // -- Flatten
+pub use binary::*;
 pub use chat_message::*;
 pub use chat_options::*;
 pub use chat_req_response_format::*;
 pub use chat_request::*;
 pub use chat_response::*;
 pub use chat_stream::*;
-pub use binary::*;
 pub use content_part::*;
 pub use message_content::*;
 pub use tool::*;
